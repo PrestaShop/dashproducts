8334276dd161be5ac49e47a6bc4ab5e72a547652
Thibaud Chauviere <thibaud.chauviere@gmail.com>
2014-06-12 15:27:53 +0200

    [*] MO : dashproducts - changed default limit for recent orders

--- dashproducts.php
+++ dashproducts.php
@@ -106,7 +106,7 @@ class DashProducts extends Module
 			array('title' => $this->l('Action'), 'class' => 'text-center'),
 		);
 
-		$limit = (int)Configuration::get('DASHPRODUCT_NBR_SHOW_LAST_ORDER') ? (int)Configuration::get('DASHPRODUCT_NBR_SHOW_LAST_ORDER') : null;
+		$limit = (int)Configuration::get('DASHPRODUCT_NBR_SHOW_LAST_ORDER') ? (int)Configuration::get('DASHPRODUCT_NBR_SHOW_LAST_ORDER') : 10;
 		$orders = Order::getOrdersWithInformations($limit);
 
 		$body = array();
