b9c3c7c440ac6ae7b2e67003ce89d4e3b6c881ff
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-03-02 11:19:25 +0100

    // Version updated to v0.3.3

--- dashproducts.php
+++ dashproducts.php
@@ -33,7 +33,7 @@ class DashProducts extends Module
 	{
 		$this->name = 'dashproducts';
 		$this->tab = 'dashboard';
-		$this->version = '0.3.2';
+		$this->version = '0.3.3';
 		$this->author = 'PrestaShop';
 
 		$this->push_filename = _PS_CACHE_DIR_.'push/activity';
