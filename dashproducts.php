545dd1482cfbf66a8255d6391a2168382f707904
Maxime Biloé <maxime.biloe@gmail.com>
2017-01-11 17:11:47 +0100

    // Version updated to v1.0.0

--- dashproducts.php
+++ dashproducts.php
@@ -33,7 +33,7 @@ class DashProducts extends Module
 	{
 		$this->name = 'dashproducts';
 		$this->tab = 'dashboard';
-		$this->version = '0.3.5';
+		$this->version = '1.0.0';
 		$this->author = 'PrestaShop';
 
 		$this->push_filename = _PS_CACHE_DIR_.'push/activity';
