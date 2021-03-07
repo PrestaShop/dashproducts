e44a70a780d1766cfaef2ca7b8c82037d62f41cb
ks129 <45097959+ks129@users.noreply.github.com>
2021-03-07 09:37:27 +0200

    Bump required PrestaShop version to 1.7.7

--- dashproducts.php
+++ dashproducts.php
@@ -46,7 +46,7 @@ class dashproducts extends Module
         parent::__construct();
         $this->displayName = $this->trans('Dashboard Products', array(), 'Modules.Dashproducts.Admin');
         $this->description = $this->trans('Enrich your stats, display a table of your latest orders and a ranking of your products.', array(), 'Modules.Dashproducts.Admin');
-        $this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = array('min' => '1.7.7.0', 'max' => _PS_VERSION_);
     }
 
     public function install()
