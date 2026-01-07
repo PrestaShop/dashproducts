978f45b5299485afe1aea7f990dc4714ad188748
mattgoud <matthias.goudjil@prestashop.com>
2026-01-07 16:22:41 +0100

    release: add version 2.2.1

--- dashproducts.php
+++ dashproducts.php
@@ -33,7 +33,7 @@ class dashproducts extends Module
     {
         $this->name = 'dashproducts';
         $this->tab = 'administration';
-        $this->version = '2.2.0';
+        $this->version = '2.2.1';
         $this->author = 'PrestaShop';
 
         parent::__construct();
